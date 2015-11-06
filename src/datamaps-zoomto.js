896c37c02329426b78b7d1d7c5dc1ef24eb1d006
Joel Lubrano <jlubrano@lexmark.com>
2015-11-06 15:30:33 -0500

    No reprojection...getting somewhat close to desired behavior.

--- src/datamaps-zoomto.js
+++ src/datamaps-zoomto.js
@@ -36,8 +36,9 @@
       return 'translate(' + x + ',' + y + ')';
     };
 
-    var genScaleStr = function(scaleFactor) {
-      return 'scale(' + scaleFactor + ')';
+    var genScaleStr = function(x, y) {
+      if(y === undefined) y = x;
+      return 'scale(' + x + ',' + y + ')';
     };
     
     var overrideProps = function(orig, addition) {
@@ -80,6 +81,11 @@
       };
 
       var transformStr = genTranslateStr(t.x, t.y) + ' ' + genScaleStr(s);
+
+      datamapsSubunits().attr('data-zoomto-scale', s)
+        .attr('data-zoomto-tx', t.x)
+        .attr('data-zoomto-ty', t.y)
+      ;
 
       datamapsSubunits().transition()
         .duration(options.transition.duration)
@@ -125,13 +131,97 @@
       self.draw();
     };
 
+    var parseTranslate = function(transformStr) {
+      var translateRegex = /translate\(\s*(-?\d*\.?\d*)\s*,\s*(-?\d*\.?\d*)\s*\)/gi;
+      var translateResult = translateRegex.exec(transformStr);
+      var t = {
+        x: 0,
+        y: 0
+      };
+      if(translateResult) {
+        t.x = +translateResult[1];
+        t.y = +translateResult[2];
+      }
+      console.log(t);
+      return t;
+    };
+
+    var parseScale = function(transformStr) {
+      console.log(transformStr);
+      var s = { x: 1, y: 1 };
+      var singleScaleRegex = /scale\(\s*(\d*\.?\d*)\s*\)/gi;
+      var scaleResult = singleScaleRegex.exec(transformStr);
+      if(scaleResult) {
+        s.x = s.y = scaleResult[1];
+      } else {
+        var multiScaleRegex = /scale\(\s*(\d*\.?\d*)\s*,\s*(\d*\.?\d*)\s*\)/gi;
+        scaleResult = multiScaleRegex.exec(transformStr);
+        if(scaleResult) {
+          s.x = scaleResult[1];
+          s.y = scaleResult[2];
+        }
+      }
+      return s;
+    };
+
+    var parseTransform = function(transformStr) {
+      var transform = {
+        scale: parseScale(transformStr),
+        translate: parseTranslate(transformStr)
+      };
+      console.log(transform);
+      return transform;
+    };
+
+    var resize = function() {
+      if(this.options.responsive) {
+        var newsize = this.options.element.clientWidth;
+        var svg = d3.select(this.options.element).select('svg');
+        var oldsize = svg.attr('data-width');
+        var resizeScaleFactor = newsize / oldsize;
+        svg.selectAll('g').attr('transform', function() {
+          var sel = d3.select(this);
+          var transformStr = sel.attr('transform');
+          var transform = parseTransform(transformStr);
+          var zoomScale = sel.attr('data-zoomto-scale');
+          transform.scale.x = resizeScaleFactor * zoomScale;
+          transform.scale.y = resizeScaleFactor * zoomScale;
+
+          transformStr = genTranslateStr(
+            transform.translate.x,
+            transform.translate.y
+          ) + genScaleStr(
+            transform.scale.x,
+            transform.scale.y
+          );
+
+          return transformStr;
+        });
+      }
+    };
+
     // execute zoom
+    console.log(self);
+    self.resize = resize.bind(self);
     options = overrideProps(defaultOptions, options);
     animateZoom();
-    setTimeout(reprojectMap, options.transition.duration);
+    // setTimeout(reprojectMap, options.transition.duration);
   };
 
   if(Datamap !== undefined) {
+    // redefine resize prototype
+    // Datamap.prototype.resize = function() {
+    //   var self = this;
+    //   if(self.options.responsive) {
+    //     var newsize = self.options.element.clientWidth;
+    //     var oldsize = d3.select(self.options.element).select('svg').attr('data-width');
+    //     d3.select(self.options.element)
+    //       .select('svg')
+    //       .selectAll('g')
+    //       .attr('transform', 'scale(' + (newsize / oldsize) + ')')
+    //     ;
+    //   }
+    // };
     var dm = new Datamap({ element: document.createElement('div') });
     dm.addPlugin(PLUGIN_NAME, zoomtoPlugin);
   }
